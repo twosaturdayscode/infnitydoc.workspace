1082e10d9afa1446bacb7c14fda9b3d8ef9e1fd3
Ali Sbtan <ali.sbtan@vas.it>
2024-01-08 01:15:34 +0100

    Add useInterval hook and hide WhatsappButton after 6 seconds

--- app/routes/($lang)._public._index/components/whatsapp-button.tsx
+++ app/routes/($lang)._public._index/components/whatsapp-button.tsx
@@ -1,8 +1,24 @@
+import { useInterval } from '@src/hooks'
+import { useState } from 'react'
+
 export function WhatsappButton() {
+  const [isVisible, setIsVisible] = useState(true)
+
+  useInterval(() => {
+    setIsVisible(false)
+  }, 6000)
+
   return (
     <div className="fixed bottom-8 right-8 z-50 translate-y-0">
       <a href="https://wa.me/393888770022">
-        <img src="/assets/wa.png" alt="Whatsapp" className="h-12 w-12" />
+        <div className='flex items-center gap-4'>
+          {isVisible && (
+            <div className='py-2 px-4 rounded-full bg-stone-100'>
+              <span className='text-xl text-stone-700'>Ti serve aiuto?</span>
+            </div>
+          )}
+          <img src="/assets/wa.png" alt="Whatsapp" className="h-12 w-12" />
+        </div>
       </a>
     </div>
   )
